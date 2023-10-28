69873df32586c69f067525605f7bd70c78f732b9
Eric Doering <ericbenjamindoering@gmail.com>
2023-10-28 13:21:39 -0400

    finding specific characters in url

--- webscraper.py
+++ webscraper.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
 with open("index.html", "r") as f:
     doc = BeautifulSoup(f, "html.parser")
@@ -13,5 +14,8 @@ tag.string = "Beautiful Soup Title Change"
 url = "https://www.guitarcenter.com/Ibanez/TOD10N-Tim-Henson-Signature-Nylon-Acoustic-Electric-Guitar-Black-Flat-1500000386789.gc?icid=421588"
 guitar_page = requests.get(url)
 
+guitar_page_formatted = BeautifulSoup(guitar_page.text, "html.parser")
 
-print(guitar_page.text)
+prices = guitar_page_formatted.find_all(text=re.compile(r'\$'))
+
+print(prices)
