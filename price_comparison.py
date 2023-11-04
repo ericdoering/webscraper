aecad7d685e78f9c61977db847002d105b712072
Eric Doering <ericbenjamindoering@gmail.com>
2023-11-04 13:23:01 -0400

    successfully obtained New Egg 3080 graphics card price

--- price_comparison.py
+++ price_comparison.py
@@ -4,15 +4,28 @@ import re
 
 product = input("What product do you want to search for? ")
 
-url ="https://nuphy.com/collections/keyboards/products/{product}"
+prices = {
+    "New Egg": "",
+    "Best Buy": "",
+    "Amazon": ""
+}
+
+# New Egg
+
+url = f"https://www.newegg.com/p/pl?d={product}&N=4131"
 page = requests.get(url).text
 doc = BeautifulSoup(page, "html.parser")
 
 
+item_info_div = doc.find(["div"], class_="item-info") 
 
-items = doc.find_all(text=re.compile(product))
+text = item_info_div.get_text() 
+price = None
 
-print(items)
+import re
+price_match = re.search(r'\$(\d+\.\d{2})', text)
 
-# for item in items:
-#     print(item)
+if price_match:
+    price = price_match.group(0)
+    prices["New Egg"] = price
+
