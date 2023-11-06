f1611292f1e33c79df3dd7bafb1c22219c78f95f
Eric Doering <ericbenjamindoering@gmail.com>
2023-11-05 21:57:30 -0500

    new product site added

--- price_comparison.py
+++ price_comparison.py
@@ -6,7 +6,7 @@ product = input("What product do you want to search for? ")
 
 prices = {
     "New Egg": "",
-    "Best Buy": "",
+    "Item Emporium": "",
     "Amazon": ""
 }
 
@@ -29,3 +29,22 @@ if price_match:
     price = price_match.group(0)
     prices["New Egg"] = price
 
+# Item Emporium (Fake E-Commerce Site)
+
+with open("item_emporium.html", "r") as f:
+    doc = BeautifulSoup(f, "html.parser")
+    
+
+product_name_tag = doc.find('h2', text='NVIDIA GeForce RTX 3080 Graphics Card')
+
+if product_name_tag:
+
+    price_tag = product_name_tag.find_next('p', class_='price')
+
+    if price_tag:
+        price = price_tag.text
+        prices["Item Emporium"] = price
+else:
+    print("Product name tag not found in the HTML.")
+
+print(prices)
